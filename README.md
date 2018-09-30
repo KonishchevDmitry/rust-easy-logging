11c9487eece0c4412928a7130b22e0ec8d985771
Dmitry Konishchev <konishchev@gmail.com>
2018-09-30 23:00:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+easy-logging
+============
+
+An easy way to get logging working in your command line tool. Suitable for simple CLI and prototyping.
+
+Requires a single function call and provides colored logging to stdout/stderr out of the box.
+
+### Usage
+
+Add to your `Cargo.toml`:
+
+```toml
+[dependencies]
+easy-logging = "*"
+```
+
+#### Example:
+
+```rust
+extern crate easy_logging;
+#[macro_use] extern crate log;
+
+fn main() {
+    easy_logging::init(module_path!(), log::Level::Info).unwrap();
+    debug!("Test debug message.");
+    info!("Test info message.");
+}
+```
+
+#### Output with enabled info level:
+```
+I: Test info message.
+```
+
+#### Output with enabled debug level:
+```
+[22:29:18.084] [   main.rs:006] D: Test debug message.
+[22:29:18.085] [   main.rs:007] I: Test info message.
+```
---
+++ src/lib.rs
@@ -0,0 +1,174 @@
+extern crate ansi_term;
+extern crate atty;
+extern crate chrono;
+extern crate fern;
+#[macro_use] extern crate lazy_static;
+extern crate log;
+
+use std::fmt;
+use std::io::{self, Write};
+use std::sync::Mutex;
+
+use ansi_term::Color;
+use fern::{Dispatch, FormatCallback};
+use log::{Level, LevelFilter, SetLoggerError};
+
+lazy_static! {
+    static ref GLOBAL_CONTEXT: Mutex<Option<String>> = Mutex::new(None);
+    static ref OUTPUT_MUTEX: Mutex<()> = Mutex::new(());
+}
+
+pub struct GlobalContext {
+}
+
+impl GlobalContext {
+    pub fn new(name: &str) -> GlobalContext {
+        let context_string = format!("[{}] ", name);
+
+        {
+            let mut context = GLOBAL_CONTEXT.lock().unwrap();
+            if context.is_some() {
+                panic!("An attempt to set a nested global context");
+            }
+            *context = Some(context_string);
+        }
+
+        GlobalContext{}
+    }
+
+    fn get() -> String {
+        GLOBAL_CONTEXT.lock().unwrap().as_ref().map(Clone::clone).unwrap_or_else(String::new)
+    }
+}
+
+impl Drop for GlobalContext {
+    fn drop(&mut self) {
+        *GLOBAL_CONTEXT.lock().unwrap() = None;
+    }
+}
+
+pub fn init(module_name: &'static str, level: Level) -> Result<(), SetLoggerError> {
+    let debug_mode = level >= Level::Debug;
+
+    let stdout_dispatcher =
+        configure_formatter(Dispatch::new(), debug_mode, atty::is(atty::Stream::Stdout))
+        .filter(|metadata| {metadata.level() >= Level::Info})
+        .chain(io::stdout());
+
+    let stderr_dispatcher =
+        configure_formatter(Dispatch::new(), debug_mode, atty::is(atty::Stream::Stderr))
+        .filter(|metadata| {metadata.level() < Level::Info})
+        .chain(io::stderr());
+
+    Dispatch::new()
+        .level(if debug_mode {
+            LevelFilter::Warn
+        } else {
+            LevelFilter::Off
+        })
+        .level_for(module_name, level.to_level_filter())
+        .chain(stdout_dispatcher)
+        .chain(stderr_dispatcher)
+        .apply()
+}
+
+fn configure_formatter(dispatcher: Dispatch, debug_mode: bool, colored_output: bool) -> Dispatch {
+    if debug_mode {
+        dispatcher.format(move |out, message, record| {
+            let level = record.level();
+            let level_name = get_level_name(level);
+            let time = chrono::Local::now().format("[%T%.3f]");
+
+            let file = if let (Some(mut file), Some(line)) = (record.file(), record.line()) {
+                let mut file_width = 10;
+                let mut line_width = 3;
+                let mut line_extra_width = line / 1000;
+
+                while line_extra_width > 0 && file_width > 0 {
+                    line_width += 1;
+                    file_width -= 1;
+                    line_extra_width /= 10;
+                }
+
+                if file.starts_with("src/") {
+                    file = &file[4..];
+                }
+
+                if file.len() > file_width {
+                    file = &file[file.len() - file_width..]
+                }
+
+                format!(" [{file:>file_width$}:{line:0line_width$}]",
+                        file=file, file_width=file_width, line=line, line_width=line_width)
+            } else {
+                String::new()
+            };
+
+            if colored_output {
+                let level_color = get_level_color(level);
+                write_log(out, level, format_args!(
+                    "{color_prefix}{time}{file} {level}: {context}{message}{color_suffix}",
+                    color_prefix=level_color.prefix(), time=time, file=file, level=level_name,
+                    context=GlobalContext::get(), message=message, color_suffix=level_color.suffix()
+                ));
+            } else {
+                write_log(out, level, format_args!(
+                    "{time}{file} {level}: {context}{message}",
+                    time=time, file=file, level=level_name, context=GlobalContext::get(),
+                    message=message
+                ));
+            }
+        })
+    } else {
+        dispatcher.format(move |out, message, record| {
+            let level = record.level();
+            let level_name = get_level_name(level);
+
+            if colored_output {
+                let level_color = get_level_color(level);
+                write_log(out, level, format_args!(
+                    "{color_prefix}{level}: {context}{message}{color_suffix}",
+                    color_prefix=level_color.prefix(), level=level_name,
+                    context=GlobalContext::get(), message=message, color_suffix=level_color.suffix()
+                ));
+            } else {
+                write_log(out, level, format_args!("{level}: {context}{message}",
+                    level=level_name, context=GlobalContext::get(), message=message));
+            }
+        })
+    }
+}
+
+fn get_level_color(level: Level) -> Color {
+    match level {
+        Level::Error => Color::Red,
+        Level::Warn  => Color::Yellow,
+        Level::Info  => Color::Green,
+        Level::Debug => Color::Cyan,
+        Level::Trace => Color::Purple,
+    }
+}
+
+fn get_level_name(level: Level) -> &'static str {
+    match level {
+        Level::Error => "E",
+        Level::Warn  => "W",
+        Level::Info  => "I",
+        Level::Debug => "D",
+        Level::Trace => "T",
+    }
+}
+
+fn write_log(out: FormatCallback, level: Level, formatted_message: fmt::Arguments) {
+    // Since we write into stdout and stderr we should guard any write with a mutex to not get the
+    // output interleaved.
+    let _lock = OUTPUT_MUTEX.lock();
+
+    out.finish(formatted_message);
+
+    let _ = if level >= Level::Info {
+        io::stdout().flush()
+    } else {
+        io::stderr().flush()
+    };
+}
